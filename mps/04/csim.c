e361a956ecff1e6172641bdaa67286facb77cfee
Triet Phan <trietphan85@gmail.com>
2017-04-11 19:09:00 -0500

    Working on cache lab

--- mps/04/csim.c
+++ mps/04/csim.c
@@ -1,7 +1,49 @@
 #include "cachelab.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+
+typedef struct {
+  char valid;
+  long tag;
+  int lru;
+} line_t;
+
+typedef struct {
+  line_t *lines;
+} set_t;
+
+typedef struct {
+  set_t *sets;
+  int setNum;
+  int lineNum;
+} cache_t;
 
 int main()
 {
     printSummary(0, 0, 0);
     return 0;
+}
+
+void help_messages(char *argv[]) {
+  printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
+  printf("Options:\n");
+  printf("  -h         Print this help message.\n");
+  printf("  -v         Optional verbose flag.\n");
+  printf("  -s <num>   Number of set index bits.\n");
+  printf("  -E <num>   Number of lines per set.\n");
+  printf("  -b <num>   Number of block offset bits.\n");
+  printf("  -t <file>  Trace file.\n\n");
+  printf("Examples:\n");
+  printf("  linux>  %s -s 4 -E 1 -b 4 -t traces/yi.trace\n", argv[0]);
+  printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
+}
+
+void cache_init(cache_t *cache, int s, int E){
+  cache->setNum = (2 << s);
+  cache->lineNum = E;
+  cache->sets = (set *)malloc(cache->setNum * sizeof(set));
+  
 }
