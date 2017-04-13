f1c5211dbaf5b298b26c81045ed8da6715336f19
Triet Phan <trietphan85@gmail.com>
2017-04-13 16:23:21 -0500

    Finish Part 1

--- mps/04/csim.c
+++ mps/04/csim.c
@@ -7,7 +7,7 @@
 
 typedef struct {
   char valid;
-  long tag;
+  int tag;
   int lru; //store least recently used for eviction
 } line_t;
 
@@ -21,21 +21,24 @@ typedef struct {
   int num_lines;
 } cache_t;
 
+int hits = 0, misses = 0, evics = 0;
+
 void help_messages(char *argv[]);
 int cache_init(cache_t *cache, int s, int E);
-void process_each_line(long long address);
-void read_trace_file(char *trace_name);
+void process_each_line(long address, int b, int s, cache_t *cache);
+void read_trace_file(char *trace_name, int b, int s, cache_t *cache);
 int process_args(int argc, char *argv[], int *s, int *E, int *b, char **t);
+int update_lru(cache_t *cache, int setBits, int line_num);
+int extract_bits(long address, int start, int end);
 
 int main(int argc, char *argv[]) {
   int s = 0, E = 0, b = 0;
-  int hits = 0, misses = 0, evics = 0;
   char *t = NULL;
   cache_t cache;
   
   process_args(argc, argv, &s, &E, &b, &t);
   cache_init(&cache, s, E);
-  read_trace_file(t);
+  read_trace_file(t, b, s, &cache);
   printSummary(hits, misses, evics);
   return 0;
 }
@@ -65,29 +68,64 @@ int cache_init(cache_t *cache, int s, int E){
     cache->sets[i].lines = (line_t *)malloc(E * sizeof(line_t));
     for (j = 0; j < cache->num_lines; j++) {
       cache->sets[i].lines[j].valid = 0;
-      cache->sets[i].lines[j].tag = 0;
-      cache->sets[i].lines[j].lru = 0;
     }
   }
   return 0;
 }
 
-int extract_bits(int address, int start, int end) {
+int extract_bits(long address, int start, int end) {
   // Returns [start, end) bits from address
   int mask = (1 << (end - start)) - 1;
   return (address >> start) & mask;
 }
 
 /* Update the least recently use for eviction */
-int lru_update(cache_t cache, int setbits, int cacheLine) {
+int update_lru(cache_t *cache, int setBits, int line_num) {
+  int i;
+  for (i = 0; i<cache->num_lines; i++) {
+    if (cache->sets[setBits].lines[i].valid &&
+        cache->sets[setBits].lines[i].lru > cache->sets[setBits].lines[line_num].lru) {
+      --cache->sets[setBits].lines[i].lru;
+    }
+  }
+  cache->sets[setBits].lines[line_num].lru = cache->num_lines;
   return 0;
 }
 
-void process_each_line(long long address) {
+void process_each_line(long address, int b, int s, cache_t *cache) {
+  int setBits = extract_bits(address, b, b + s);
+  int tagBits = extract_bits(address, b + s, 31);
+  int i;
   
+  for (i=0; i<cache->num_lines; i++) {
+    if (cache->sets[setBits].lines[i].valid == 1 &&
+        cache->sets[setBits].lines[i].tag == tagBits) {
+      hits++;
+      update_lru(cache, setBits, i);
+      return;
+    }
+  }
+  misses++;
+  for (i = 0; i < cache->num_lines; i++) {
+    if (cache->sets[setBits].lines[i].valid == 0) {
+      cache->sets[setBits].lines[i].tag = tagBits;
+      cache->sets[setBits].lines[i].valid = 1;
+      update_lru(cache, setBits, i);
+      return;
+    }
+  }
+  evics++;
+  for (i = 0; i < cache->num_lines; i++) {
+    if (cache->sets[setBits].lines[i].lru == 1) {
+      cache->sets[setBits].lines[i].valid = 1;
+      cache->sets[setBits].lines[i].tag = tagBits;
+      update_lru(cache, setBits, i);
+      return;
+    }
+  }
 }
 
-void read_trace_file(char *trace_name) {
+void read_trace_file(char *trace_name, int b, int s, cache_t *cache) {
   char buff[100];
   FILE *tn = fopen(trace_name, "r");
   if (tn == NULL) {
@@ -95,12 +133,15 @@ void read_trace_file(char *trace_name) {
     return;
   }
   while (fgets(buff, 100, tn)) {
-    unsigned long long address = 0;
-    unsigned length = 0;
+    unsigned long address = 0;
+    unsigned size = 0;
     if (buff[1] == 'M' || buff[1] == 'S' || buff[1] == 'L') {
-      sscanf(buff+2, "%llx,%u", &address, &length);
-      /* printf("%c %llx,%u\n", buff[1], address, length); */
-      process_each_line(address);
+      sscanf(buff+2, "%lx,%u", &address, &size);
+      /* printf("%c %lx,%u\n", buff[1], address, size); */
+      process_each_line(address, b, s, cache);
+    }
+    if (buff[1] == 'M') {
+      process_each_line(address, b, s, cache);
     }
   }
   fclose(tn);
